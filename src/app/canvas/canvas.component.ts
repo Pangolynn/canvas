f3120d45d845f556a458f45b598cfd9e0ac2da06
Sammilynn <hollow71@students.rowan.edu>
2018-01-07 23:14:22 -0500

    added new examples, added comments, modified some exisiting examples

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -9,42 +9,45 @@ export class CanvasComponent implements OnInit {
 
   canvas: HTMLCanvasElement;
   context: CanvasRenderingContext2D;
-  width = '300px';
-  height = '300px';
   constructor() { }
 
   ngOnInit() {
-    this.width = '300px';
-    this.height = '300px';
     this.setupCanvas();
-    //this.drawRectangles();
+    // this.drawRectangles();
     // this.drawTriangles();
     // this.drawSmiley();
     // this.drawArcs();
     // this.drawQuadtraticBezier();
     // this.drawCubicBezier();
-
+    // this.colorPalette();
+    //  this.colorPaletteCircles();
+    // this.alphaCircles();
+    // this.drawCircle();
+    // this.rgbaRectangles('255', '0', '150');
     // this.drawPacMan();
-    this.path2D();
+    // this.path2D();
+    // this.drawLineWidth();
+    this.drawLineCap();
   }
 
+  // Gets the canvas and the CanvasRenderingContext2D (the context)
   setupCanvas(): void {
     this.canvas = <HTMLCanvasElement>document.getElementById('tutorial');
     this.context = this.canvas.getContext('2d');
   }
 
+  // Demonstrates fillRect(), clearRect(), & strokeRect()
+  // methods of creating rectangles
   drawRectangles(): void {
-
     this.context.fillStyle = 'rgba(0, 0, 200, 0.5)';
     this.context.fillRect(30, 30, 50, 50);
-
     this.context.fillRect(25, 25, 100, 100);
     this.context.clearRect(45, 45, 60, 60);
     this.context.strokeRect(50, 50, 50, 50);
   }
 
+  // Demonstrates how to creat triangles using lineTo()
   drawTriangles(): void {
-    // Filled Triangle
     this.context.fillStyle = 'rgba(100, 0, 200, 0.7)';
     this.context.beginPath();
     this.context.moveTo(25, 25);
@@ -52,7 +55,6 @@ export class CanvasComponent implements OnInit {
     this.context.lineTo(25, 105);
     this.context.fill();
 
-    // Stroked Triangle
     this.context.beginPath();
     this.context.moveTo(110, 110);
     this.context.lineTo(110, 30);
@@ -61,36 +63,75 @@ export class CanvasComponent implements OnInit {
     this.context.stroke();
   }
 
+  // Demonstrates how to make a smiley face using arc()
   drawSmiley(): void {
     this.context.beginPath();
-    // C = π· 2r = 2πr.
 
+    // A full circle is 360° = 2π
+    this.context.fillStyle = 'rgb(250, 255, 0)';
     this.context.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-    this.context.moveTo(110, 75);
-    this.context.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
-    this.context.moveTo(65, 65);
-    this.context.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
-    this.context.moveTo(95, 65);
-    this.context.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
-    this.context.stroke();
+    this.context.fill();
+
+    // Half a circle is 180° = π
+    this.context.fillStyle = 'rgb(0, 0, 0)';
+    this.context.beginPath();
+    this.context.arc(75, 75, 35, 0, Math.PI, false); // Mouth
+    this.context.fill();
+
+    this.context.fillStyle = 'rgb(190, 80, 120)';
+    this.context.beginPath();
+    this.context.arc(75, 102, 20, 0, Math.PI, false);
+    this.context.fill();
+
+
+    this.context.fillStyle = 'rgb(0, 0, 0)';
+    this.context.beginPath();
+    this.context.arc(55, 65, 10, 0, Math.PI, true); // Left eye outer
+    this.context.fill();
+
+    this.context.beginPath();
+    this.context.arc(95, 65, 10, 0, Math.PI, true); // Right eye outer
+    this.context.fill();
+
+    this.context.fillStyle = 'rgb(90, 200, 100)';
+    this.context.beginPath();
+    this.context.arc(55, 65, 5, 0, Math.PI, true); // Left eye inner
+    this.context.fill();
+
+
+    this.context.fillStyle = 'rgb(90, 200, 100)';
+    this.context.beginPath();
+    this.context.arc(95, 65, 5, 0, Math.PI, true); // Right eye inner
+    this.context.fill();
+
+
   }
 
+  /*Demonstrates how to make a circle using lineTo
+  instead of arc. The circle is divided into 20 equal
+  sections. The smaller the section, the smoother the circle.
+  */
   drawCircle(): void {
     const step = 2 * Math.PI / 20;
-    const h = 150;
-    const k = 150;
-    const r = 50;
+    const h = 25;
+    const k = 25;
+    const r = 20;
 
     this.context.beginPath();
 
     for (let theta = 0; theta < 2 * Math.PI; theta += step) {
-      const x = h + r * Math.cos(theta);
-      const y = k - r * Math.sin(theta);  // the y axis is inverted, the positive y
-                                        // is down the screen not up.
+      let x = h + r * Math.cos(theta);
+      let y = k - r * Math.sin(theta);  // the y axis is inverted, the positive y
+                                          // is down the screen not up.
+      this.context.lineTo(x, y);
 
     }
+    this.context.closePath(); // complete the circle
+    this.context.stroke(); // draw the lines
   }
 
+  // Demonstrates various applications of the arc() method
+  // to create different circles/partial circles.
   drawArcs(): void {
     for (let i = 0; i < 4; i++) {
       for (let j = 0; j < 3; j++ ) {
@@ -112,7 +153,6 @@ export class CanvasComponent implements OnInit {
       }
     }
 
-
     this.context.beginPath();
     this.context.lineTo(45, 25);
     this.context.lineTo(55, 25);
@@ -120,6 +160,8 @@ export class CanvasComponent implements OnInit {
     this.context.stroke();
   }
 
+  // Demonstrates how the quadraticCurveTo can create
+  // organic objects with quadratic beziers, forms a speech bubble.
   drawQuadtraticBezier(): void {
     this.context.beginPath();
     this.context.moveTo(75, 25);
@@ -132,6 +174,8 @@ export class CanvasComponent implements OnInit {
     this.context.stroke();
   }
 
+  // Demonstrates how the bezierCurveTo can create
+  // organic objects with cubic beziers, forms a heart.
   drawCubicBezier(): void {
     this.context.beginPath();
     this.context.moveTo(75, 40);
@@ -145,11 +189,9 @@ export class CanvasComponent implements OnInit {
     this.context.fill();
   }
 
-
+  // Draws a replication of the corner of the Pacman game
+  // Best viewed on a 150x150 canvas
   drawPacMan() {
-    // this.width = '150px';
-    // this.height = '150px';
-
     this.roundedRect(this.context, 12, 12, 150, 150, 15);
     this.roundedRect(this.context, 19, 19, 150, 150, 9);
     this.roundedRect(this.context, 53, 53, 49, 33, 10);
@@ -157,6 +199,7 @@ export class CanvasComponent implements OnInit {
     this.roundedRect(this.context, 135, 53, 49, 33, 10);
     this.roundedRect(this.context, 135, 119, 25, 49, 10);
 
+    // This is pacman
     this.context.beginPath();
     this.context.arc(37, 37, 13, Math.PI / 7, -Math.PI / 7, false);
     this.context.lineTo(31, 37);
@@ -213,7 +256,6 @@ export class CanvasComponent implements OnInit {
   }
 
   // A utility function to draw a rectangle with rounded corners.
-
   roundedRect(ctx, x, y, width, height, radius) {
     ctx.beginPath();
     ctx.moveTo(x, y + radius);
@@ -228,11 +270,13 @@ export class CanvasComponent implements OnInit {
     ctx.stroke();
   }
 
+  // Demonstrates how to create a Path2D object
+  // and reuse it in a variable.
   path2D(): void {
-    var rectangle = new Path2D();
+    const rectangle = new Path2D();
     rectangle.rect(10, 10, 50, 50);
 
-    var circle = new Path2D();
+    const circle = new Path2D();
     circle.moveTo(125, 35);
     circle.arc(100, 35, 25, 0, 2 * Math.PI);
 
@@ -240,6 +284,118 @@ export class CanvasComponent implements OnInit {
     this.context.fill(circle);
   }
 
+  // Demonstrates fillStyle method to create rows/cols
+  // of slightly different colored filled in rectangles
+  colorPalette(): void {
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 6; j++) {
+        this.context.fillStyle = 'rgb(' + Math.floor(200 - 20 * i) + ', ' +
+          Math.floor(290 - 20 * j) + ', 0)';
+          this.context.fillRect(j * 25, i * 25, 25, 25);
+      }
+    }
+  }
 
+  // Demonstrates strokeStyle method to create rows/cols
+  // of slightly different colored outlines of circles
+  colorPaletteCircles(): void {
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 6; j++) {
+        this.context.strokeStyle = 'rgb(0, ' + (255 - 42 * i) + ', ' +
+          (255 - 42 * j);
+        this.context.beginPath();
+        this.context.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, Math.PI * 2, true);
+        this.context.stroke();
+      }
+    }
+  }
+
+  // Demonstrates the globalAlpha method of transparency
+  // A semi-transparent circle on top of 4 squares
+  alphaCircles(): void {
+    this.context.fillStyle = '#FD0';
+    this.context.fillRect(0, 0, 75, 75);
+    this.context.fillStyle = '#6C0';
+    this.context.fillRect(75, 0, 75, 75);
+    this.context.fillStyle = '#09F';
+    this.context.fillRect(0, 75, 75, 75);
+    this.context.fillStyle = '#F30';
+    this.context.fillRect(75, 75, 75, 75);
+    this.context.fillStyle = '#FFF';
+
+    this.context.globalAlpha = 0.2;
+
+    for (let i = 0; i < 7; i++) {
+      this.context.fillStyle = 'rgb(255, 197, 218)';
+      this.context.beginPath();
+      this.context.arc(75, 75, 10 + 10 * i, 0, Math.PI * 2, true);
+
+      this.context.fill();
+    }
+  }
+
+  // Demonstrates transparency using rgba()
+  // Add in rgb values to change the inner square color.
+  // Default is 255-255-255 (white).
+  rgbaRectangles(r: string = '255', g: string = '255', b: string = '255'): void {
+    this.context.fillStyle = 'rgb(250, 200, 0)';
+    this.context.fillRect(0, 0, 150, 37.5);
+    this.context.fillStyle = 'rgb(100, 200, 0)';
+    this.context.fillRect(0, 37.5, 150, 37.5);
+    this.context.fillStyle = 'rgb(0, 150, 250)';
+    this.context.fillRect(0, 75, 150, 37.5);
+    this.context.fillStyle = 'rgb(250, 50, 0)';
+    this.context.fillRect(0, 112.5, 150, 37.5);
+
+    for (var i = 0; i < 10; i++) {
+      this.context.fillStyle =
+        `rgba(${r}, ${g}, ${b}, ${(i + 1) / 10})`;
+      for (var j = 0; j < 4; j++) {
+        this.context.fillRect(5 + i * 14, 5 + j * 37.5, 14, 27.5);
+      }
+    }
+  }
+
+  // Demonstrates the lineWidth() property for stroke thickness
+  // The `if` section removes line widths that are not crisp
+  // Comment it out to see the difference
+  drawLineWidth(): void {
+    for (let i = 0; i < 20; i++) {
+      if (i % 2 === 0 ) {
+        continue;
+      }
+      this.context.lineWidth = 1 + i;
+      this.context.beginPath();
+      this.context.moveTo(5 + i * 20, 5);
+      this.context.lineTo(5 + i * 20, 100);
+      this.context.stroke();
+    }
+  }
+
+  // Demonstrates the three types of lineCaps:
+  // butt, round, square
+  drawLineCap(): void {
+    const lineCap = ['butt', 'round', 'square'];
+
+    // guides
+    this.context.strokeStyle = '#5fa';
+    this.context.beginPath();
+    this.context.moveTo(10, 10);
+    this.context.lineTo(140, 10);
+    this.context.moveTo(10, 140);
+    this.context.lineTo(140, 140);
+    this.context.stroke();
+
+    this.context.strokeStyle = 'black';
+    for (let i = 0; i < lineCap.length; i++ ) {
+      this.context.lineWidth = 15;
+      this.context.lineCap = lineCap[i];
+      this.context.beginPath();
+      this.context.moveTo(25 + i * 50, 10);
+      this.context.lineTo(25 + i * 50, 140);
+      this.context.stroke();
+    }
+
+  }
 }
 
